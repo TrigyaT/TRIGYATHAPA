1e4bd8f5de82abd9ed2383fba23a9d6a665d992b
Trigya Thapa <107242264+TrigyaT@users.noreply.github.com>
2024-11-23 18:38:36 +0545

    Add files via upload
    Swapping Variables

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace index2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 10, num2 = 20;
+            Console.WriteLine($"Before Swapping={num1},{num2}");
+            num1 = num1 * num2;
+            num2 = num1 / num2;
+            num1 = num1 / num2;
+            Console.WriteLine($"After Swapping={num1},{num2}");
+        }
+    }
+}
