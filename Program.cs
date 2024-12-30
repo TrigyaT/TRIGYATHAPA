b8d8b04c555af63e241569bce9b12d4e114f864a
Trigya Thapa <107242264+TrigyaT@users.noreply.github.com>
2024-12-30 08:33:59 +0545

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,21 +1,112 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;// this must be imported
 
-namespace index2
+namespace AdoBasic
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int num1 = 10, num2 = 20;
-            Console.WriteLine($"Before Swapping={num1},{num2}");
-            num1 = num1 * num2;
-            num2 = num1 / num2;
-            num1 = num1 / num2;
-            Console.WriteLine($"After Swapping={num1},{num2}");
+            display();
+            update();
+        }
+
+        static void display()
+        {
+            string connectionString = "Data Source=TRIGYA-PC\\SQLEXPRESS;Initial Catalog=HamroStoreDB;Integrated Security=True;TrustServerCertificate=True";
+            SqlConnection con = new SqlConnection(connectionString);
+            try
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("select * from Products", con);
+                SqlDataReader sdr = cmd.ExecuteReader();
+                while (sdr.Read())
+                {
+
+                    Console.WriteLine(sdr.GetInt32(0));
+                    Console.WriteLine(sdr.GetString(1));
+                    Console.WriteLine(sdr.GetString(2));
+                    Console.WriteLine(sdr.GetInt32(3));
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally { con.Close(); }
+
+        }
+        static void insert()
+        {
+            string connectionString = "Data Source=TRIGYA-PC\\SQLEXPRESS;Initial Catalog=HamroStoreDB;Integrated Security=True;TrustServerCertificate=True";
+            SqlConnection con = new SqlConnection(connectionString);
+            try
+            {
+                con.Open();
+
+                string name, description;
+                int price = 0;
+                Console.WriteLine("enter product name: ");
+                name = Console.ReadLine();
+                Console.WriteLine("enter product description: ");
+                description = Console.ReadLine();
+                price = Int32.Parse(Console.ReadLine());
+
+                SqlCommand cmd = new SqlCommand($"insert into Products(Name,Description,Price)values('{name}','{description}',{price})", con);
+                cmd.ExecuteNonQuery();
+
+
+                Console.WriteLine("inserted successfully........................");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        static void update()
+        {
+            string connectionString = "Data Source=TRIGYA-PC\\SQLEXPRESS;Initial Catalog=HamroStoreDB;Integrated Security=True;TrustServerCertificate=True";
+            SqlConnection con = new SqlConnection(connectionString);
+            try
+            {
+                con.Open();
+
+                Console.WriteLine("Enter product ID to update: ");
+                int id = Int32.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter new product name: ");
+                string name = Console.ReadLine();
+
+                Console.WriteLine("Enter new product description: ");
+                string description = Console.ReadLine();
+
+                Console.WriteLine("Enter new product price: ");
+                int price = Int32.Parse(Console.ReadLine());
+
+                SqlCommand cmd = new SqlCommand($"update Products set Name='{name}', Description='{description}', Price={price} where Id={id}", con);
+                cmd.ExecuteNonQuery();
+
+                Console.WriteLine("Updated successfully...");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                con.Close();
+            }
         }
     }
 }
